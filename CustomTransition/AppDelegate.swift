5b62ede53589cf5dcad443860ed24aad75e3a178
iossocket <avx302@gmail.com>
2016-04-02 10:43:15 +0800

    init project

---
+++ CustomTransition/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CustomTransition
+//
+//  Created by ZhuXueliang on 4/2/16.
+//  Copyright © 2016 IOSSOCKET. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ CustomTransition/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CustomTransition
+//
+//  Created by ZhuXueliang on 4/2/16.
+//  Copyright © 2016 IOSSOCKET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
